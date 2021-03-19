20d896376f5bac4ba9487a9d940eefcdc804fd01
yeon-kyu <45001093+yeon-kyu@users.noreply.github.com>
2021-03-19 19:39:26 +0900

    Fix Graph_순위.cpp

--- 프로그래머스/그래프_순위.cpp
+++ 프로그래머스/그래프_순위.cpp
@@ -3,68 +3,56 @@
 #include <iostream>
 using namespace std;
 
-vector<int> parent[101];
-vector<int> child[101];
-int cnt = 0;
-int N;
-int visited[101] = {false};
-int getParentCnt(int pos){
-    visited[pos] = true;
-    int s = parent[pos].size();
-    if(s==0){
-        return 0;
-    }
-    int ret = 0;
-    for(int i=0;i<s;i++){
-        if(visited[parent[pos][i]] == false)
-            ret += getParentCnt(parent[pos][i])+1;
-    }
-    return ret;
-}
-int getChildCnt(int pos){
-    visited[pos] = true;
-    int s = child[pos].size();
-    if(s==0){
-        return 0;
-    }
-    int ret= 0;
-    for(int i = 0;i<s;i++){
-        if(visited[child[pos][i]] == false)
-            ret+=getChildCnt(child[pos][i])+1;
-    }
-    return ret;
-}
+vector<int> upNode[101];
+vector<int> downNode[101];
+bool visited[101] = { false };
 
-void checkSure(int pos){
-    
-    int a = getParentCnt(pos);
-    int b = getChildCnt(pos);
-    
-    cout<<pos<<"의 부모수 : "<<a<<", 자식수 : "<<b<<endl;
-    
-    if( a+b+1 == N){
-        cnt++;
-    }
-    
+int upCount(int pos) {
+	visited[pos] = true;
+	int s = upNode[pos].size();
+	int res = 0;
+	for (int i = 0; i < s; i++) {
+		if (visited[upNode[pos][i]] == false) {
+			res++;
+			//cout << pos << "에서 " << upNode[pos][i] << "추가" << endl;
+			res += upCount(upNode[pos][i]);
+		}
+	}
+	return res;
+}
+int downCount(int pos) {
+	visited[pos] = true;
+	int s = downNode[pos].size();
+	int res = 0;
+	for (int i = 0; i < s; i++) {
+		if (visited[downNode[pos][i]] == false) {
+			res++;
+			res += downCount(downNode[pos][i]);
+		}
+	}
+	return res;
 }
 
 int solution(int n, vector<vector<int>> results) {
-    int answer = 0;
-    N=n;
-    for(int i=0;i<results.size();i++){
-        int a = results[i][0];
-        int b= results[i][1];
-        parent[b].push_back(a);
-        child[a].push_back(b);
-    }
-    
-    for(int i=1;i<=n;i++){
-        for(int k=1;k<=n;k++){
-            visited[k] = false;
-        }
-        checkSure(i);
-    }
-    answer = cnt;
-    
-    return answer;
+	int answer = 0;
+
+	int size = results.size();
+	for (int i = 0; i < size; i++) {
+		upNode[results[i][1]].push_back(results[i][0]);
+		downNode[results[i][0]].push_back(results[i][1]);
+	}
+
+	for (int i = 1; i <= n; i++) {
+		for (int k = 0; k <= n; k++) {
+			visited[k] = false;
+		}
+		int parentCount = upCount(i);
+		int childCount = downCount(i);
+		
+        if (parentCount + childCount == n - 1) {
+			answer++;
+		}
+	}
+
+	return answer;
 }
