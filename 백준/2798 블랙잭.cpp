e8721fe1446564ec3b18e35c2e11beeeb682b472
yeon-kyu <45001093+yeon-kyu@users.noreply.github.com>
2021-04-12 01:10:48 +0900

    Update 2798 블랙잭.cpp

--- 백준/2798 블랙잭.cpp
+++ 백준/2798 블랙잭.cpp
@@ -8,27 +8,23 @@ int num[101];
 
 int maxi;
 
-void brute(int pos,int cnt, int val) {
-	cnt++;
-	if (cnt == 3) {
-		int temp = val + num[pos];
-		if (temp > maxi && temp<=M) {
-			maxi = temp;
-			//cout << temp<< " "<<pos<<endl;
+void brute(int pos, int cnt, int val) {
+	if (cnt == 3) { //그 다음 노드 가기 직전에 검사한다
+		if (val > maxi && val <= M) {
+			maxi = val;
 		}
-		//cout << temp << " " << pos << endl;
-		if(pos+1<N)
-			brute(pos + 1, cnt - 1, val);
+		return;
 	}
-	
-	else if (pos + 1 < N) {
-		brute(pos + 1, cnt - 1, val);
-		brute(pos + 1, cnt, val + num[pos]);
+	if (cnt >= 4 || pos >= N || val>M) {
+		return;
 	}
+
+	brute(pos + 1, cnt + 1, val + num[pos]);
+	brute(pos + 1, cnt, val);
 }
 
 int main() {
-	
+
 	cin >> N >> M; // N < 100, M<300 000
 	for (int i = 0; i < N; i++) {
 		cin >> num[i];
@@ -38,9 +34,6 @@ int main() {
 	maxi = 0;
 
 	brute(0, 0, 0);
-	for (int i = 0; i <= N - 3; i++) {
-		//brute(i,0,0);
-	}
 	cout << maxi;
 
 }
